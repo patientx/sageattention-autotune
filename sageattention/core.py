815c42d95d36ef9bc43e89e71ddb91b7894e9729
patientx <patient.x@gmail.com>
2026-07-04 23:03:35 +0300

    Refactor backend selection for SAGEATTN

--- sageattention/core.py
+++ sageattention/core.py
@@ -1,7 +1,7 @@
 import importlib
-import os
 
 from .triton_attn import sageattn_qk_int8_pv_fp16_triton
+from .utils import _env_flag_enabled
 
 try:
     importlib.import_module(f"{__package__}._qattn_sm80")
@@ -10,8 +10,7 @@ except (ImportError, OSError):
 else:
     from .cuda_attn import sageattn_qk_int8_pv_fp16_cuda
 
-SAGEATTN_BACKEND = os.getenv("SAGEATTN_BACKEND", "").lower()
-if SAGEATTN_BACKEND == "triton":
+if _env_flag_enabled("SAGEATTN_TRITON_BACKEND") or sageattn_qk_int8_pv_fp16_cuda is None:
     sageattn = sageattn_qk_int8_pv_fp16_triton
 else:
     sageattn = sageattn_qk_int8_pv_fp16_cuda
